d7f34004c12b5b9ceff17d0e463efe4e453243e6
Srinivaslancius <srinivas@lanciussolutions.com>
2017-11-08 11:51:12 +0530

    new chnages fiels

---
+++ manage_webmaster/admin_includes/config.php
@@ -0,0 +1,29 @@
+<?php
+error_reporting(0);
+session_start();
+date_default_timezone_set("Asia/Kolkata");
+ 
+$setcon = 1;
+if($setcon == 2) {
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "loanmamu";
+} else {
+	$servername = "192.168.0.114";	
+	$username = "root";
+	$password = "root";
+	$dbname = "sai_meghana_dance";
+}  
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}  
+ 
+$base_url = "http://localhost/sai_meghana_dance/";
+
+
+?>
