e6cfd68449ece51b2a0a8b0ce73fb585a51d417b
srinivaslancius <srinivas@lanciussolutions.com>
2017-11-10 16:22:47 +0530

    Changed front end to dynamic all pages

--- manage_webmaster/content_pages.php
+++ manage_webmaster/content_pages.php
@@ -25,7 +25,7 @@
                     <td><?php echo $row['title'];?></td>
                     <td><?php echo substr(strip_tags($row['description']), 0,150);?></td>
                     <td><?php if ($row['status']==0) { echo "<span class='label label-outline-success check_active open_cursor' data-incId=".$row['id']." data-status=".$row['status']." data-tbname='content_pages'>Active</span>" ;} else { echo "<span class='label label-outline-info check_active open_cursor' data-status=".$row['status']." data-incId=".$row['id']." data-tbname='content_pages'>In Active</span>" ;} ?></td>
-                    <td> <a href="edit_content_pages.php?uid=<?php echo $row['id']; ?>"><i class="zmdi zmdi-edit"></i></a> &nbsp; <a href="#"><i class="zmdi zmdi-eye zmdi-hc-fw" data-toggle="modal" data-target="#<?php echo $row['id']; ?>" class=""></i></a></td>
+                    <td> <a href="edit_content_pages.php?uid=<?php echo $row['id']; ?>"><i class="zmdi zmdi-edit"></i></a> &nbsp; <!-- <a href="#"><i class="zmdi zmdi-eye zmdi-hc-fw" data-toggle="modal" data-target="#<?php echo $row['id']; ?>" class=""></i></a> --></td>
                     <!-- Open Modal Box  here -->
                     <div id="<?php echo $row['id']; ?>" class="modal fade" tabindex="-1" role="dialog">
                       <div class="modal-dialog">
